3a5e575a1ca227d84e550bb374746a9f1d21ac4c
jhonbuenogl <jbueno@guruverso.com>
2024-12-27 08:46:28 -0500

    Commit 008

--- src/app/(api)/api/reports/api/get-client-data/route.ts
+++ src/app/(api)/api/reports/api/get-client-data/route.ts
@@ -8,26 +8,9 @@ export const GET = async () => {
 
     console.log(clientResponse.data.clientData);
 
-    // const htmlClients = clientResponse.data.clientData.map(client => (
-    //   <div></div>
-    // ))
-
     const users = await prisma.user.findMany();
 
     console.log(users);
-
-    // const mailOptions: SendMailOptions = {
-    //   from: "sap@cbs.pe", // Dirección de correo del remitente
-    //   to: "jbueno@guruverso.com", // Dirección de correo del destinatario
-    //   subject: "Prueba de correo desde Node.js", // Asunto del correo
-    //   html: `<div>Hola mundo</div>`,
-    //   // Puedes adjuntar archivos si lo necesitas:
-    //   // attachments: [
-    //   //   { filename: 'documento.pdf', path: 'ruta/a/tu/archivo.pdf' }
-    //   // ]
-    // };
-
-    // await nodemailerTransporter.sendMail(mailOptions);
 
     return NextResponse.json(
       { message: "Correo enviado exitosamente", users },
