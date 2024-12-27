ce942e68ab74d38ee15265a1553bcc4b4ddc4b64
jhonbuenogl <jbueno@guruverso.com>
2024-12-27 08:54:26 -0500

    Commit 010

--- src/app/(api)/api/users/api/get-all/route.ts
+++ src/app/(api)/api/users/api/get-all/route.ts
@@ -5,12 +5,7 @@ export const GET = async () => {
   try {
     const users = await prisma.user.findMany();
 
-    console.log(users);
-
-    return NextResponse.json(
-      { message: "Correo enviado exitosamente", users },
-      { status: 200 }
-    );
+    return NextResponse.json({ users }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error }, { status: 500 });
