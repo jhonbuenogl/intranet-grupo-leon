b42ed5aeda575d1743a33ff677f7815a8975dcba
jhonbuenogl <jbueno@guruverso.com>
2025-01-04 12:04:09 -0500

    042

--- src/actions/get-pdf.ts
+++ src/actions/get-pdf.ts
@@ -34,12 +34,19 @@ export const getVoucherPDFPath = async ({
 
   const pdfBase64 = response.data;
 
+  const pdfDirPath = path.join(process.cwd(), `/public/pdf/`);
+
+  await fsp.rm(pdfDirPath, { recursive: true, force: true });
+  await fsp.mkdir(pdfDirPath, { recursive: true });
+
   const outputPath = path.join(
     process.cwd(),
-    `/public/vouchers/${docType}-${serie}-${correlative}.pdf`
+    `/public/pdf/${docType}-${serie}-${correlative}.pdf`
   );
 
   const filename = `${docType}-${serie}-${correlative}.pdf`;
+
+  fs.writeFileSync(outputPath, Buffer.from(pdfBase64, "base64"));
 
   return { outputPath, filename, pdfBase64 };
 };
