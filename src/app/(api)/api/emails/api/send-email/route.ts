895d125826bc7b810ba31b4d37dc1ba5a6746e98
jhonbuenogl <jbueno@guruverso.com>
2025-01-10 13:58:44 -0500

    Commit 064

--- src/app/(api)/api/emails/api/send-email/route.ts
+++ src/app/(api)/api/emails/api/send-email/route.ts
@@ -2,7 +2,7 @@ import { sendChangePasswordEmail } from "@/actions/send-change-password-email";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  await sendChangePasswordEmail("jbueno@guruverso.com");
+  await sendChangePasswordEmail("jbueno@guruverso.com", "fjdlaskj");
 
   return NextResponse.json({ message: "Api funcionando" });
 };
